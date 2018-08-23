29c4eb9bd66a4f101a67c2627dfea8d23ae6d78f
Thiago Polycarpo <polycarpo.thiago@gmail.com>
2018-08-23 19:03:23 -0300

    adicionando a função carrega arquivo

--- Biblioteca/biblioteca.cpp
+++ Biblioteca/biblioteca.cpp
@@ -7,28 +7,35 @@
 #include <stdio.h>
 #include <string.h>
 
+//defines
+#define TAM_STRUCT 5
+
+//structs
 struct livro {
-    char isbn[13];
+    //int  usado=0; // 0 para não usado, 1 para usado
+    char isbn[14];
     char titulo[50];
     char autor[50];
     char ano[5];
-}lr;
+}lr[TAM_STRUCT];
+
 
 //funcoes
 int abrir_arquivo(FILE **p_arq, char *nome_arq);
 void criar_arquivo(FILE **p_arq, char *nome_arq);
 void fechar_arquivo(FILE **p_arq);
 void menu();
+void carrega_arquivo();
 char pega_registro(FILE *p_arq, char *p_reg);
 
 int main()
 {
-	FILE *arq;
-	char *pch, registro[119], tam_reg, cadastro[]="biblioteca.bin";
+	//FILE *arq;
+	char *pch, registro[119], tam_reg;
  	int aberto;
   
   menu();
-  //abrir arquivo, criar, exibir e fechar
+  //abrir arquivo, criar, exibir e fechar -- teste pra ver
 	/*aberto = abrir_arquivo(&arq, cadastro);
 	
 	while(fread(&lr, sizeof(livro), 1, arq)){
@@ -38,9 +45,9 @@ int main()
 		printf("\n%s\n", lr.ano);
 	}
 	
-	//if(!aberto) criar_arquivo(arq, "Biblioteca.");
+	//if(!aberto) criar_arquivo(arq, "Biblioteca.");*/
 	
- 	fechar_arquivo(&arq);*/
+ 	//fechar_arquivo(&arq);
   return 0;
 }
 
@@ -97,12 +104,73 @@ void menu()
     case 1: break;
     case 2: break;
     case 3: break;
-    case 4: break;
+    case 4: carrega_arquivo(); break;
     case 5: break;
     default: printf("\nOpção inválida!");
   }
 }
 
+//carrega arquivo biblioteca.bin ou remove.bin  -- falta gerar a marcação de registros usados ou não
+void carrega_arquivo()
+{
+  char arq_cadastro[]="biblioteca.bin", arq_remove[]="remove.bin";
+  FILE *arq;
+  int i, aberto,resp;
+
+  do
+  {
+    system("clear");
+    printf("Digite 1- biblioteca.bin e 2-remove.bin\n: ");
+    scanf("%d", &resp);
+  }while(resp < 0 || resp > 1);
+  //se resp == 1 carrega biblioteca.bin, senão remove.bin
+  if(resp)
+  {
+    //abre arquivo biblioteca, carrega em vetor de struct 
+    aberto = abrir_arquivo(&arq, arq_cadastro);
+    
+    if(aberto)
+    {
+      i=0;
+      while(fread(&lr[i], sizeof(livro), 1, arq))
+      {
+        i++;
+      }
+     /*i=0; // exibe o vetor de struct do arquivo biblioteca.bin
+      while(i<TAM_STRUCT)
+      {
+        printf("\n%s", lr[i].isbn);
+        printf("\n%s", lr[i].titulo);
+        printf("\n%s", lr[i].autor);
+        printf("\n%s\n", lr[i].ano);
+        i++;
+      }*/
+    }
+    fechar_arquivo(&arq);
+  }
+  else
+  {
+    //abre arquivo remove, carrega em vetor de struct -- pode usar o mesmo vetor?! 
+    aberto = abrir_arquivo(&arq, arq_remove);
+      
+    if(aberto)
+    {
+      i=0;
+      while(fread(&lr, sizeof(livro), 1, arq))
+      {
+        i++;
+      }/*
+      i=0;
+      while(fread(&lr, sizeof(livro), 1, arq)) //exibe o vetor de struct do arquivo remove.bin
+      {
+        printf("\n%s", lr.isbn);
+        i++;
+      }*/
+    }
+    fechar_arquivo(&arq);
+  }
+  menu();   
+}
 //devolve o tamanho dos registros
 char pega_registro(FILE *p_arq, char *p_reg)
 {
