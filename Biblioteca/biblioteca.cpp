463014d0d1f13e64a6a48e94bc952cc66962d3c5
Bruno Domene <brdomene@gmail.com>
2018-08-23 06:02:06 -0300

    Inserindo código para exibir itens do arquivo

--- Biblioteca/biblioteca.cpp
+++ Biblioteca/biblioteca.cpp
@@ -7,46 +7,44 @@
 #include <stdio.h>
 #include <string.h>
 
-//funcoes
-int abrir_arquivo(FILE *p_arq, char *nome_arq);
-void criar_arquivo(FILE *p_arq, char *nome_arq);
-void fechar_arquivo(FILE *p_arq);
-char pega_registro(FILE *p_arq, char *p_reg);
+struct livro {
+    char isbn[14];
+    char titulo[50];
+    char autor[50];
+    char ano[5];
+}lr;
 
+//funcoes
+int abrir_arquivo(FILE **p_arq, char *nome_arq);
+void criar_arquivo(FILE **p_arq, char *nome_arq);
+void fechar_arquivo(FILE **p_arq);
+char pega_registro(FILE *p_arq, char *p_reg);
 
 int main()
 {
 	FILE *arq;
-	char *pch, registro[60], tam_reg, cadastro[] = "cadastro.cpp";
-  int aberto;
+	char *pch, registro[119], tam_reg;
+ 	int aberto;
   
-	aberto = abrir_arquivo(arq, cadastro); 
-
-
-  if(!aberto) criar_arquivo(arq, cadastro);
+	aberto = abrir_arquivo(&arq, "biblioteca.bin");
 	
-
- 	/*fseek(arq,0,0);
-  	tam_reg = pega_registro(arq,registro);
-  	while (tam_reg > 0)
-   	{
-    	pch = strtok(registro,"|");
-    	while (pch != NULL)
-      	{
-        	printf("%s\n",pch);
-	        pch = strtok(NULL,"|");
-      	}
-	    printf("\n");
-	    tam_reg = pega_registro(arq,registro);  
-   	} */
- 	fechar_arquivo(arq);
+	while(fread(&lr, sizeof(livro), 1, arq)){
+		printf("\n%s", lr.isbn);
+		printf("\n%s", lr.titulo);
+		printf("\n%s", lr.autor);
+		printf("\n%s\n", lr.ano);
+	}
+	
+	//if(!aberto) criar_arquivo(arq, "Biblioteca.");
+	
+ 	fechar_arquivo(&arq);
   return 0;
 }
 
 //abre arquivos
-int abrir_arquivo(FILE *p_arq, char *nome_arq)
+int abrir_arquivo(FILE **p_arq, char nome_arq[])
 {
-  if((p_arq = fopen(nome_arq,"r+b")) == NULL)
+  if((*p_arq = fopen(nome_arq,"r+b")) == NULL)
   {
     printf("\nimpossivel abrir o arquivo");
     return 0;
@@ -55,9 +53,9 @@ int abrir_arquivo(FILE *p_arq, char *nome_arq)
 } 
 
 //cria arquivos
-void criar_arquivo(FILE *p_arq, char *nome_arq)
+void criar_arquivo(FILE **p_arq, char nome_arq[])
 {
-  if((p_arq = fopen(nome_arq,"a+b")) == NULL)
+  if((*p_arq = fopen(nome_arq,"a+b")) == NULL)
   {
     printf("impossivel criar o arquivo");
     exit(0);
@@ -65,9 +63,9 @@ void criar_arquivo(FILE *p_arq, char *nome_arq)
 }
 
 //fecha arquivos
-void fechar_arquivo(FILE *p_arq)
+void fechar_arquivo(FILE **p_arq)
 {
-  fclose(p_arq);
+  fclose(*p_arq);
 }
 
 //devolve o tamanho dos registros
